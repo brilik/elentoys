0d6e5c9ad1f5017acd8e044e7ff35b861e29ebae
brilik <megabrilik@gmail.com>
2019-05-20 10:19:33 +0300

    Page blog allright.

--- tpl-page/blog.php
+++ tpl-page/blog.php
@@ -4,6 +4,8 @@
  */
 
 get_template_part('tpl-part/head');
+
+global $post;
 ?>
 
 <div class="main-wrapper blog-wrapper">
@@ -12,86 +14,74 @@ get_template_part('tpl-part/head');
 
         <main class="content">
             <section class="blog-section">
-                <div class="blog-section__top" data-mob-href="img/slide-with-bg.jpg" data-tab-href="img/slide-with-bg-tab.jpg">
+                <?php $left = get_field('bckg'); ?>
+                <div class="blog-section__top"
+                     style="background-image: url('<?php echo $left['banner']; ?>')"
+                     data-mob-href="<?php echo $left['banner_mob']; ?>"
+                     data-tab-href="<?php echo $left['banner_table']; ?>">
                     <div class="blog-top__wrap">
                         <h1 class="blog-top__title">
-                            Блог
+                            <?php the_title(); ?>
                         </h1>
                     </div>
                 </div>
                 <div class="blog-section__price">
-                    <div class="wrapper">   
+                    <div class="wrapper">
                         <ul class="blog-price__list">
-                            <li class="blog-price__item">
-                                <div class="blog-price__pict">
-                                    <img src="img/blog-s.png" alt="">
-                                </div>
-                                <a href="" class="blog-price__img">
-                                    <img src="img/blog8.png" alt="">
-                                </a>
-                                <div class="blog-price__description">
-                                    <h4 class="blog-description__title">
-                                       <a href="">
-                                        Пошив большой плюшевой игрушки - или как <br>
-я шила слона из “Следствие ведут колобки”
-                                       </a>
-                                    </h4>
-                                    <div class="blog-description__date">
-                                        25. 12. 2018
-                                    </div>
-                                    <div class="blog-description__text">
-                                        С самого начала решила не скупиться на размер “Слон, так слон!”, поэтому выкройку пришлось склеивать из частей (исключительный случай)...
 
-                                    </div>
-                                    <a href="#" class="btn btn_pink"><span class="btn__icon">.</span>Читать<span class="btn__icon btn__icon_reverse">.</span></a>
-                                </div>
-                            </li>
-                            <li class="blog-price__item">
-                           
-                                <a href="" class="blog-price__img">
-                                    <img src="img/blog9.png" alt="">
-                                </a>
-                                <div class="blog-price__description">
-                                    <h4 class="blog-description__title">
-                                      <a href="">
-                                       8 кроссовок на пауке <br>
-или как я поборола фобию                </a>
-                                    </h4>
-                                    <div class="blog-description__date">
-                                        25. 12. 2018
-                                    </div>
-                                    <div class="blog-description__text">
-                                        Сшить огромного паука из видеоигры - необычный и интересный заказ, который лично для меня стал настоящим вызовом. Чтобы изготовить Сквиттера, я подружилась с миром паукообразных...
+                            <?php
+                            $count = get_field('count_news');
+                            $args = array(
+                                'numberposts' => $count,
+                                'orderby'     => 'date',
+                                'order'       => 'DESC',
+                                'post_status' => 'publish',
+                                'include'     => array(),
+                                'exclude'     => array(),
+                                'meta_key'    => '',
+                                'meta_value'  =>'',
+                                'post_type'   => 'post',
+                                'suppress_filters' => true,
+                            );
+                            $count=0;
 
+                            foreach( get_posts($args) as $post ) : setup_postdata($post); $count++; ?>
+
+                                <li class="blog-price__item">
+
+                                    <?php if($count === 1 ): ?>
+                                    <div class="blog-price__pict">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog-s.png" alt="">
                                     </div>
-                                    <a href="#" class="btn btn_pink"><span class="btn__icon">.</span>Читать<span class="btn__icon btn__icon_reverse">.</span></a>
-                                </div>
-                            </li>
-                            <li class="blog-price__item">
-                         
-                                <a href="" class="blog-price__img">
-                                    <img src="img/blog10.png" alt="">
-                                </a>
-                                <div class="blog-price__description">
-                                    <h4 class="blog-description__title">
-                                       <a href="">
-                                        Как я шила свою первую sleepy toy
+                                    <?php endif; ?>
+
+                                    <a href="<?php the_permalink(); ?>" class="blog-price__img">
+                                        <?php the_post_thumbnail(); ?>
+                                    </a>
+                                    <div class="blog-price__description">
+                                        <h4 class="blog-description__title">
+                                            <a href="<?php the_permalink(); ?>">
+                                                <?php the_title(); ?>
+                                            </a>
+                                        </h4>
+                                        <div class="blog-description__date">
+                                            <?php the_date('d. m. Y'); ?>
+                                        </div>
+                                        <div class="blog-description__text">
+                                            <?php the_content(); ?>
+                                        </div>
+                                        <a href="<?php the_permalink(); ?>" class="btn btn_pink">
+                                            <span class="btn__icon">.</span>Читать<span class="btn__icon btn__icon_reverse">.</span>
                                         </a>
-                                    </h4>
-                                    <div class="blog-description__date">
-                                        25. 12. 2018
                                     </div>
-                                    <div class="blog-description__text">
-                                        Для sleepy toy (спящая игрушка) самое главное небольшой размер (должна помещаться на плече и в кармане хозяина) и приятная на ощупь фактура, чтобы на нее было приятно прилечь…
+                                </li>
 
-                                    </div>
-                                    <a href="#" class="btn btn_pink"><span class="btn__icon">.</span>Читать<span class="btn__icon btn__icon_reverse">.</span></a>
-                                </div>
-                            </li>
+                            <?php endforeach; ?>
+
                         </ul>
                     </div>
                 </div>
-                <div class="blog-section__img"><img src="img/blog11.png" alt=""></div>
+                <div class="blog-section__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog11.png" alt=""></div>
             </section>
         </main>
 
