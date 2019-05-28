3f24729df75eb8869b35f8474b45bafd9c2c22c9
brilik <megabrilik@gmail.com>
2019-05-28 15:59:50 +0300

    Added background for other devices

--- tpl-page/delivery.php
+++ tpl-page/delivery.php
@@ -14,7 +14,9 @@ get_template_part('tpl-part/head');
 		<main class="content">
 
 			<section class="blog-section blog-section_feedback">
-				<div class="blog-section__top" style="background-image:url('<?php echo get_field('banner_img')['url']; ?>')">
+				<div class="blog-section__top" style="background-image:url('<?php echo get_field('banner_img')['url']; ?>')"
+                     data-mob-href="<?php echo get_field('banner_img_mob')['url']; ?>"
+                     data-tab-href="<?php echo get_field('banner_img_tab')['url']; ?>">
 					<div class="feedback-bg">
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/delivery-main-1.png" alt="">
 					</div>
