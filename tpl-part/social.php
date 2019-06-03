e5bc581b508e41ab19dbd5a514f3d2ab338c7e6f
brilik <megabrilik@gmail.com>
2019-06-03 18:25:50 +0300

    Modification function. Link whatsapp edited on open chat with text

--- tpl-part/social.php
+++ tpl-part/social.php
@@ -5,14 +5,16 @@
  *
  * @param $ulClass
  * @param int $isPopup приминмает только 1, 2 или 0 (по умолчанию). 1 - в попапе, 2 - в секции
- * @param int $arrShow принимает массив социалок, которые нужно вывести
+ * @param int $arrShow принимает массив социалок, которые нужно вывести. Example: telegram, inst, vk, viber
+ * @param string $productName нужно использовать функцию get_the_title().
  * @return string
  */
-function elentoys_the_social($ulClass, $isPopup = 0, $arrShow = 0)
+function elentoys_the_social($ulClass, $isPopup = 0, $arrShow = 0, $productName = '')
 {
 
     $out = (string)'';
-    $count = 0;
+    $count = $key = 0;
+    $textOrder = (string)"Добрый день! \n Я хочу заказать \"$productName\"";
 
     if (get_field('vertical', 'options')):
 
@@ -27,7 +29,7 @@ function elentoys_the_social($ulClass, $isPopup = 0, $arrShow = 0)
                 if ($fileName == 'twitter' || $fileName == 'skype' || $fileName == '' || $fileName == 'wp' || $fileName == 'viber') continue;
 
                 $out .= '<li class="new-soc__item item' . ++$count . '">
-                                <a href="' . get_sub_field('link') . '" class="new-soc__link" target="_blank"git >
+                                <a href="' . get_sub_field('link') . '" class="new-soc__link" target="_blank">
                                     <img src="' . get_template_directory_uri() . '/assets/img/social/' . $fileName . '2.svg"
                                          alt="">
                                 </a>
@@ -46,8 +48,6 @@ function elentoys_the_social($ulClass, $isPopup = 0, $arrShow = 0)
             }
 
         } elseif ($isPopup === 0) {
-
-            $key = 0;
 
             if( $arrShow !== 0 ) {
 
@@ -74,6 +74,43 @@ function elentoys_the_social($ulClass, $isPopup = 0, $arrShow = 0)
                 }
 
             }
+
+        } elseif ($isPopup === 3) {
+
+            if( $arrShow !== 0 ) {
+
+                while (has_sub_field('vertical', 'options')) {
+
+                    $key = array_search(get_sub_field('icon'), $arrShow);
+
+                    $link = $phone = get_sub_field('link');
+
+                    if ('whatsapp' === $arrShow[$key]) {
+
+                        $phone = elentoys_the_phone_clear($phone);
+                        $link = "https://wa.me/$phone?text=".urlencode($textOrder);
+                    }
+
+                    if (false !== $key) {
+                        $out .= '<li class="social__item">
+                        <a href="'. $link .'" class="social__link" target="_blank">
+                        <i class="icon-' . $arrShow[$key] . '"></i></a></li>';
+                    }
+
+                }
+
+            } else {
+
+                while (has_sub_field('vertical', 'options')) {
+
+                    $out .= '<li class="social__item">
+                    <a href="' . get_sub_field('link') . '" class="social__link" target="_blank">
+                    <i class="icon-' . get_sub_field('icon') . '"></i></a></li>';
+
+                }
+
+            }
+
         }
 
         $out .= '</ul>';
