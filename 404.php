859501317c2985f7f4a52b9fc6c1bd9760dcc241
brilik <megabrilik@gmail.com>
2019-06-28 16:50:32 +0300

    Added banner and cusomis here.

--- 404.php
+++ 404.php
@@ -1,4 +1,15 @@
-<?php get_template_part('tpl-part/head'); ?>
+<?php
+get_template_part('tpl-part/head');
+$customSlider = get_field('customSlider','options');
+$toysLeft = $customSlider['left'];
+$toysRight = $customSlider['right'];
+$toysLeftTab = $customSlider['left_tab'];
+$toysRightTab = $customSlider['right_tab'];
+$toysLeftMob = $customSlider['left_mob'];
+$toysRightMob = $customSlider['right_mob'];
+$text = get_field('text', 'options');
+$btn = get_field('btn', 'options');
+?>
 
 <div class="main-wrapper wrapper-404">
 
@@ -7,15 +18,12 @@
     <main class="content content_404">
         <?php $left = get_field('404_left','options'); ?>
         <section class="section-404 block-desktop"
-                 style="background-image: url(<?php echo $left['404_banner']; ?>)"
-                 data-mob-href="<?php echo $left['404_banner_table']; ?>"
-                 data-tab-href="<?php echo $left['404_banner_mob']; ?>">
+                 data-mob-href="<?php echo $customSlider['imgMob']; ?>"
+                 data-tab-href="<?php echo $customSlider['imgTab']; ?>"
+                 style="background: <?php echo $customSlider['back_color']; ?> url('<?php echo $customSlider['imgDesc']; ?>') no-repeat;">
             <div class="wrapper">
                 <div class="section-404__wrapper">
                     <div class="section-404__box">
-                        <div class="section-404__img">
-                            <img src="<?php the_field('404_img', 'options'); ?>" alt="404-image">
-                        </div>
                         <div class="section-404__desc">
                             <h1 class="section-404__head">
                                 <span class="section-404__head-before">404</span>
@@ -27,27 +35,37 @@
                                 <span class="section-404__name"><?php _e('Страница не найдена','elentoys'); ?></span>
                                 <span class="nav__icon">У</span>
                             </h3>
-                            <p class="section-404__text">
-                                <?php the_field('text','options');?>
-                            </p>
-                            <a href="<?php echo home_url(); ?>" class="btn btn_pink">
-                                <span class="btn__icon">.</span>На главную<span class="btn__icon btn__icon_reverse">.</span>
+                            <p class="section-404__text"><?php echo $text['desk'];?></p>
+                            <a href="<?php echo home_url(); ?>" class="btn btn_pink"
+                               style="transform: translate(<?php echo $btn['position_desk']['x']; ?>px,
+                               <?php echo $btn['position_desk']['y']; ?>px)!important;">
+                                <span class="btn__icon">.</span>На главную DESKTOP<span class="btn__icon btn__icon_reverse">.</span>
                             </a>
+                            <?php if ($toysLeft['show']): ?>
+                                <img class="toy-top" src="<?php echo $toysLeft['img']['url']; ?>"
+                                     alt="toys-right"
+                                     style="transform: translate(<?php echo $toysLeft['positionX']; ?>px,
+                                     <?php echo $toysLeft['positionY']; ?>px)!important;"
+                                >
+                            <?php endif; ?>
+                            <?php if ($toysRight['show']): ?>
+                                <img class="toy-top toy-top-right" src="<?php echo $toysRight['img']['url']; ?>"
+                                     alt="<?php echo $toysRight['img']['alt']; ?>"
+                                     style="transform: translate(<?php echo $toysRight['positionX']; ?>px,
+                                     <?php echo $toysRight['positionY']; ?>px)!important;">
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
         <section class="section-404 block-table"
-                 style="background-image: url(<?php echo $left['404_banner']; ?>)"
-                 data-mob-href="<?php echo $left['404_banner_table']; ?>"
-                 data-tab-href="<?php echo $left['404_banner_mob']; ?>">
+                 data-mob-href="<?php echo $customSlider['imgMob']; ?>"
+                 data-tab-href="<?php echo $customSlider['imgTab']; ?>"
+                 style="background: <?php echo $customSlider['back_color']; ?> url('<?php echo $customSlider['imgDesc']; ?>') no-repeat;">
             <div class="wrapper">
                 <div class="section-404__wrapper">
                     <div class="section-404__box">
-                        <div class="section-404__img">
-                            <img src="<?php the_field('404_img', 'options'); ?>" alt="404-image">
-                        </div>
                         <div class="section-404__desc">
                             <h1 class="section-404__head">
                                 <span class="section-404__head-before">404</span>
@@ -59,27 +77,37 @@
                                 <span class="section-404__name"><?php _e('Страница не найдена','elentoys'); ?></span>
                                 <span class="nav__icon">У</span>
                             </h3>
-                            <p class="section-404__text">
-                                <?php the_field('text','options');?>
-                            </p>
-                            <a href="<?php echo home_url(); ?>" class="btn btn_pink">
-                                <span class="btn__icon">.</span>На главную<span class="btn__icon btn__icon_reverse">.</span>
+                            <p class="section-404__text"><?php echo $text['tab']; ?></p>
+                            <a href="<?php echo home_url(); ?>" class="btn btn_pink"
+                               style="transform: translate(<?php echo $btn['position_tab']['x']; ?>px,
+                               <?php echo $btn['position_tab']['y']; ?>px)!important;">>
+                                <span class="btn__icon">.</span>На главную TABLE<span class="btn__icon btn__icon_reverse">.</span>
                             </a>
+                            <?php if ($toysLeftTab['show']): ?>
+                                <img class="toy-top" src="<?php echo $toysLeftTab['img']['url']; ?>"
+                                     alt="<?php echo $toysLeftTab['img']['alt']; ?>"
+                                     style="transform: translate(<?php echo $toysLeftTab['positionX']; ?>px,
+                                     <?php echo $toysLeftTab['positionY']; ?>px)!important;"
+                                >
+                            <?php endif; ?>
+                            <?php if ($toysRightTab['show']): ?>
+                                <img class="toy-top toy-top-right" src="<?php echo $toysRightTab['img']['url']; ?>"
+                                     alt="<?php echo $toysRightTab['img']['alt']; ?>"
+                                     style="transform: translate(<?php echo $toysRightTab['positionX']; ?>px,
+                                     <?php echo $toysRightTab['positionY']; ?>px)!important;">
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
         <section class="section-404 block-phone"
-                 style="background-image: url(<?php echo $left['404_banner']; ?>)"
-                 data-mob-href="<?php echo $left['404_banner_table']; ?>"
-                 data-tab-href="<?php echo $left['404_banner_mob']; ?>">
+                 data-mob-href="<?php echo $customSlider['imgMob']; ?>"
+                 data-tab-href="<?php echo $customSlider['imgTab']; ?>"
+                 style="background: <?php echo $customSlider['back_color']; ?> url('<?php echo $customSlider['imgDesc']; ?>') no-repeat;">
             <div class="wrapper">
                 <div class="section-404__wrapper">
                     <div class="section-404__box">
-                        <div class="section-404__img">
-                            <img src="<?php the_field('404_img', 'options'); ?>" alt="404-image">
-                        </div>
                         <div class="section-404__desc">
                             <h1 class="section-404__head">
                                 <span class="section-404__head-before">404</span>
@@ -91,12 +119,25 @@
                                 <span class="section-404__name"><?php _e('Страница не найдена','elentoys'); ?></span>
                                 <span class="nav__icon">У</span>
                             </h3>
-                            <p class="section-404__text">
-                                <?php the_field('text','options');?>
-                            </p>
-                            <a href="<?php echo home_url(); ?>" class="btn btn_pink">
-                                <span class="btn__icon">.</span>На главную<span class="btn__icon btn__icon_reverse">.</span>
+                            <p class="section-404__text"><?php echo $text['mob']; ?></p>
+                            <a href="<?php echo home_url(); ?>" class="btn btn_pink"
+                               style="transform: translate(<?php echo $btn['position_mob']['x']; ?>px,
+                               <?php echo $btn['position_mob']['y']; ?>px)!important;">>
+                                <span class="btn__icon">.</span>На главную PHONE<span class="btn__icon btn__icon_reverse">.</span>
                             </a>
+                            <?php if ($toysLeftMob['show']): ?>
+                                <img class="toy-top" src="<?php echo $toysLeftMob['img']['url']; ?>"
+                                     alt="<?php echo $toysLeftMob['img']['alt']; ?>"
+                                     style="transform: translate(<?php echo $toysLeftMob['positionX']; ?>px,
+                                     <?php echo $toysLeftMob['positionY']; ?>px)!important;"
+                                >
+                            <?php endif; ?>
+                            <?php if ($toysLeftMob['show']): ?>
+                                <img class="toy-top toy-top-right" src="<?php echo $toysLeftMob['img']['url']; ?>"
+                                     alt="<?php echo $toysLeftMob['img']['alt']; ?>"
+                                     style="transform: translate(<?php echo $toysLeftMob['positionX']; ?>px,
+                                     <?php echo $toysLeftMob['positionY']; ?>px)!important;">
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
